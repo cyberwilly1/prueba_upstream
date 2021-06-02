48a2d1885c356494444705f4a9d6de351dac0730
Claure <cclaurev@est.emi.edu.bo>
2021-06-02 19:18:50 -0400

    commit hecho por carlos

---
+++ carlos.py
@@ -0,0 +1,1 @@
+print('archivo creado por carlos')
--- main.py
+++ main.py
@@ -8,4 +8,8 @@ def print_segundo():
 def print_first():
     print('Rachel modifico aqui')
 
+def print_carlos():
+    print('Carlos modifico aqui')
+
 print_first()
+print_carlos()
